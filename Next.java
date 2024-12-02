f44fe6c833bb599aa6639142d34a164111d8e400
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-02 15:38:43 +0530

    Add files via upload

---
+++ Next.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Next{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        System.out.println(a);
+        System.out.println(b);
+    }
+    }
