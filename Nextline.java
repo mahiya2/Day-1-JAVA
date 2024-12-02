fbb8f80e7c5c5afd844042bca42d86335cc5c80f
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-02 15:44:44 +0530

    Add files via upload

---
+++ Nextline.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Nextline{
+    public static void main(String args[]){
+        Scanner Sc=new Scanner(System.in);
+        String a=Sc.nextLine();
+        String b=Sc.nextLine();
+        System.out.print(a);
+        System.out.print(b);
+    }
+    }
