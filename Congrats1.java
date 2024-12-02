693bc7a35b30b37ac82067129ffd3e6cccfe3dd4
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-02 16:09:01 +0530

    Add files via upload

---
+++ Congrats1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Congrats1{
+    public static void main(String args[]){
+        Scanner Sc=new Scanner(System.in);
+        String a=Sc.next();
+        String b=String.format("Congraculations %s you won 5 lakhs",a);
+        System.out.println(b);
+    }
+}
